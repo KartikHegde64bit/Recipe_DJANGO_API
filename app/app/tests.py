2564ce4392a0da99c98931d0f0785da48ab3931d
Kartik Hegde <kartikhegde062@gmail.com>
2024-02-06 22:31:21 +0530

    docker-compose postgres dependency added

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,9 +3,6 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
-
-    
-
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
